e54f66622db9b27fa93dca83c372b7e074c41355
Diego Freile <dfreile51@gmail.com>
2022-12-01 22:53:36 +0100

    Ejercicio 1 mongo no terminado

--- EjerciciosMongoDB/1/index.php
+++ EjerciciosMongoDB/1/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilos.css">
     <title>Ejercicio 1 MongoDB</title>
 </head>
 <body>
@@ -20,26 +21,49 @@
         $productos = $colProductos->find()->toArray();
         $proveedores = $colProveedores->find()->toArray();
 
-        $categoriasPorId = array( 
-            
-         );
+        $arraynuevo = array();
+        $arraynuevo2 = array();
 
-        /* echo "<table>";
+        foreach($productos as $producto) {
+            foreach($categorias as $categoria) {
+                if($producto->CategoryID == $categoria->CategoryID) {
+                    $arraynuevo[] = array("id" => $producto->CategoryID, "nombre" => $categoria->CategoryName);
+                }
+            }
+        }
+
+        foreach($productos as $producto) {
+            foreach($proveedores as $proveedor) {
+                if($producto->SupplierID == $proveedor->SupplierID) {
+                    $arraynuevo2[] = array("id" => $proveedor->SupplierID, "nombre" => $proveedor->CompanyName);
+                }
+            }
+        }
+
+        /* echo "<pre>";
+        print_r($arraynuevo);
+        echo "</pre>"; */
+
+        echo "<table>";
         echo "<tr>";
         echo "<th>#</th>";
         echo "<th>Productos</th>";
+        echo "<th>ID Categoria</th>";
         echo "<th>Categoria</th>";
+        echo "<th>Proveedor</th>";
         echo "<th>Precio</th>";
         echo "</tr>";
         foreach($productos as $producto) {
             echo "<tr>";
             echo "<th>{$producto->ProductID}</th>";
             echo "<td>{$producto->ProductName}</td>";
-            echo "<td>{$categoria->CategoryName}</td>";
+            echo "<td>{$producto->CategoryID}</td>";
+            echo "<td>{$arraynuevo['nombre']}</td>";
+            echo "<td>{$arraynuevo2['nombre']}</td>";
             echo "<td>{$producto->UnitPrice}</td>";
             echo "</tr>";
         }
-        echo "</table>"; */
+        echo "</table>";
     ?>
 </body>
 </html>
